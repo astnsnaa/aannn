20284e9c41f8623909aa37ab537e31ea1b4f514a
novandri511 <45961623+novandri511@users.noreply.github.com>
2020-05-29 13:19:34 +0700

    first commit

---
+++ src/components/Test.vue
@@ -0,0 +1,82 @@
+<template>
+    <body class="font-sans h-full w-full object-cover antialiased text-gray-900 leading-normal tracking-wider object-cover" style="background-image:url('https://source.unsplash.com/1L71sPT5XKc');">
+            
+        <div class="
+            max-w-4xl 
+            h-auto 
+            py-32
+            text-white 
+            flex 
+            flex-wrap 
+            items-center 
+            mx-auto
+            my-auto">
+            
+            <!-- main text START-->
+            <div class="w-full
+            lg:w-3/5
+            rounded-lg
+            lg:rounded-1-lg
+            lg:rounded-r-none
+            shadow-2xl
+            opacity-75
+            bg-white
+            mx-6
+            lg:mx-0
+            ">
+                
+                <div class="
+                md:p-12
+                text-center
+                lg:text-left
+                ">
+                <!-- image for mobile -->
+                <img class="block lg:hidden object-cover object-top rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center" src="../assets/20200429_145620.png"/>
+                    
+                <h1 class="text-3xl font-bold text-gray-900 lg:pt-0 ">Good to see you , I'am Novan..</h1>
+                    <div class="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-teal-500 opacity-25"></div>   
+                <p class="text-gray-900 pt-4 text-base font-bold flex items-center justify-center lg:justify-start"><svg class="h-4 fill-current text-teal-700 pr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9 12H1v6a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-6h-8v2H9v-2zm0-1H0V5c0-1.1.9-2 2-2h4V2a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v1h4a2 2 0 0 1 2 2v6h-9V9H9v2zm3-8V2H8v1h4z"/></svg>Junior FrontEnd Engineer</p>
+                <p class="pt-2 text-gray-600 text-xs lg:text-sm flex items-center justify-center lg:justify-start"><svg class="h-4 fill-current text-teal-700 pr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 20a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm7.75-8a8.01 8.01 0 0 0 0-4h-3.82a28.81 28.81 0 0 1 0 4h3.82zm-.82 2h-3.22a14.44 14.44 0 0 1-.95 3.51A8.03 8.03 0 0 0 16.93 14zm-8.85-2h3.84a24.61 24.61 0 0 0 0-4H8.08a24.61 24.61 0 0 0 0 4zm.25 2c.41 2.4 1.13 4 1.67 4s1.26-1.6 1.67-4H8.33zm-6.08-2h3.82a28.81 28.81 0 0 1 0-4H2.25a8.01 8.01 0 0 0 0 4zm.82 2a8.03 8.03 0 0 0 4.17 3.51c-.42-.96-.74-2.16-.95-3.51H3.07zm13.86-8a8.03 8.03 0 0 0-4.17-3.51c.42.96.74 2.16.95 3.51h3.22zm-8.6 0h3.34c-.41-2.4-1.13-4-1.67-4S8.74 3.6 8.33 6zM3.07 6h3.22c.2-1.35.53-2.55.95-3.51A8.03 8.03 0 0 0 3.07 6z"/></svg> Indonesian -7.967348° N, 112.650497° W</p>
+                <p class="pt-8 text-sm text-gray-700">An enthusiast frontEnd Engineer with willingness to solve problem as many as I can, open-minded, self-starter, person with integrity with these scalable skills :</p>
+                    <div class=" space-x-2 flex justify-center m-0 ">
+                        <span class="text-gray-700 bg-transparent border-2 hover:border-transparent border-yellow-300 rounded-full p-1 hover:bg-yellow-700 hover:text-white mt-5 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:shadow-lg">#JAVASCRIPT </span>    
+                        <span class="text-gray-700 bg-transparent border-2 hover:border-transparent border-orange-300 rounded-full p-1 hover:bg-orange-600 hover:text-white mt-5 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:shadow-lg">#HTML5 </span>    
+                        <span class="text-gray-700 bg-transparent border-2 hover:border-transparent border-blue-300 rounded-full p-1 hover:bg-blue-600 hover:text-white mt-5 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:shadow-lg">#CSS3 </span>    
+                        <span class="text-gray-700 bg-transparent border-2 hover:border-transparent border-purple-300 rounded-full p-1 hover:bg-purple-600 hover:text-white mt-5 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:shadow-lg">#BOOTSTRAP </span>    
+                        
+                    </div>    
+                    <div class="space-x-2 flex justify-center mb-3">
+                        <span class="text-gray-700 bg-transparent border-2 hover:border-transparent border-red-300 rounded-full p-1 hover:bg-red-600 hover:text-white mt-5 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:shadow-lg">#LARAVEL </span>    
+                        <span class="text-gray-700 bg-transparent border-2 hover:border-transparent border-green-300 rounded-full p-1 hover:bg-green-600 hover:text-white mt-5 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:shadow-lg">#VUE.JS </span>    
+                        <span class="text-gray-700 bg-transparent border-2 hover:border-transparent border-yellow-300 rounded-full p-1 hover:bg-yellow-700 hover:text-white mt-5 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:shadow-lg">#ECMASCRIPT 6 </span>    
+                    </div>
+
+                    <div class="pt-3 pb-8 flex justify-around border-t space-x-10 mx-6">
+                        <button class="flex-1 bg-teal-700 hover:bg-teal-900 text-white font-bold py-2 px-4 rounded-full" onclick="window.location.href = 'https://wa.me/628971055226?text=I%20interested%20with%20your%20work';">
+                        Chat me
+                        </button> 
+
+                        <button class="flex-1 bg-teal-700 hover:bg-teal-900 text-white font-bold py-2 px-4 rounded-full" onclick="window.location.href = 'https://drive.google.com/file/d/1ELuelT4aqly5d_JIkplGX0H5RD_7LPFA/view?usp=sharing';">
+                        My Resume
+                        </button> 
+                    </div>
+                    
+                    
+                    
+
+                    <div class="mt-6 pb-16 lg:pb-0 w-4/5 lg:w-full mx-auto flex flex-wrap items-center justify-start space-x-5">
+                        <a class="link" href="https://github.com/novandri511" target="_blank" data-tippy-content="@github_handle"><svg class="h-6 fill-current text-gray-600 hover:text-teal-700" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>GitHub</title><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/></svg></a>
+                        <a class="link" href="https://www.youtube.com/channel/UCahSdLZVQhsoU9iDVsodw2w" target="_blank" data-tippy-content="@youtube_handle"><svg class="h-6 fill-current text-gray-600 hover:text-teal-700" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>YouTube</title><path d="M23.495 6.205a3.007 3.007 0 0 0-2.088-2.088c-1.87-.501-9.396-.501-9.396-.501s-7.507-.01-9.396.501A3.007 3.007 0 0 0 .527 6.205a31.247 31.247 0 0 0-.522 5.805 31.247 31.247 0 0 0 .522 5.783 3.007 3.007 0 0 0 2.088 2.088c1.868.502 9.396.502 9.396.502s7.506 0 9.396-.502a3.007 3.007 0 0 0 2.088-2.088 31.247 31.247 0 0 0 .5-5.783 31.247 31.247 0 0 0-.5-5.805zM9.609 15.601V8.408l6.264 3.602z"/></svg></a>
+                    </div>
+                </div>
+            </div>
+            <!-- main text END-->
+
+            <!-- image only START-->
+            <div class="w-full lg:w-2/5">
+                <img src="../assets/20200429_145620.png" class="rounded-none lg:rounded-lg shadow-2xl hidden lg:block "/>
+            </div>
+            <!-- image only END-->
+        </div>
+    </body>
+</template>
